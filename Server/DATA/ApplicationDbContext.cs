da45933154067c1e214021533b57b2a09b4167cd
josezapata456 <josealfredozapata44@gmail.com>
2023-12-05 13:02:07 -0400

    concliente

---
+++ Server/DATA/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp3.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BlazorApp3.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+
+        // public DbSet<NombreClase> NombreTablaEnBaseDeDatos { get; set; }
+    }
+}
+
